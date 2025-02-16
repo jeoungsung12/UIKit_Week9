d62918a548f38d1d8dda000fdcf79bd28c0554ee
jeoungsung12 <jeoungsung12@naver.com>
2025-02-17 08:38:29 +0900

    Initial Commit

---
+++ SeSAC_Week9/Scene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_Week9
+//
+//  Created by 정성윤 on 2/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
